054d3237efd7b859fb86c71858ea39e9f5efb06f
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-05-12 23:58:49 +0600

    make admin api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
@@ -28,12 +28,10 @@ function verifyJWT(req, res, next) {
     return res.status(401).send("unauthorized access");
   }
   const token = authHeader.split(" ")[1];
-
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
     if (err) {
-      return res.status(403).send({ message: "forbidded access" });
+      return res.status(403).send({ message: "forbidden access" });
     }
-
     req.decoded = decoded;
     next();
   });
@@ -170,9 +168,32 @@ async function run() {
       res.status(403).send({ accessToken: "" });
     });
 
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
   } finally {
